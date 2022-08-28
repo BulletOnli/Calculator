1f709342b5a288c4c61de298a48bf7d912e84d24
Gemmuel <gemmueldelacruz@gmail.com>
2022-08-28 16:41:30 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+let btn = document.querySelectorAll(".btn")
+let value = document.getElementById("value")
+
+for (let i=0; i < btn.length; i++ ) {
+    btn[i].addEventListener("click", function() {
+        if (this.innerHTML == "=") {
+            value.innerHTML = eval(value.innerHTML)
+        } else if (this.innerHTML == "AC") {
+            value.textContent = ""
+        } else {
+            value.innerHTML += this.innerHTML
+        }
+    })
+}
+
+let mainBg = document.getElementById("bg-image")
+const inputEl = document.querySelector(".change")
+
+
+inputEl.addEventListener("click", function() {
+    update()
+})
+
+function update() {
+    if (inputEl.checked) {
+        document.body.style.backgroundImage = "url('bg2.png')";
+    } else {
+        document.body.style.backgroundImage = "url('bgsky.png')";
+    }
+}
